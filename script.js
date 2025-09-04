37b209a232a319ad8b7d23c9320d1d34df04e101
Hrydey Bhatti <hrydeybhatti@gmail.com>
2025-09-04 16:07:04 +0100

    Created JS script and linked it to HTML file and creating code for hover text effect

---
+++ script.js
@@ -0,0 +1,9 @@
+//Create Array of Messages
+const messages = [
+    "Hi, I'm Hrydey !",
+    "Data Science & Computing Student at Birkbeck University, Univesity Of London",
+    "Building insightful solutions using data and code"
+]
+
+
+let currentIndex
